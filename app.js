c514da54aed86b67ffe2f7e62a2e81bc0e5e9c70
Zach Padovano <zacharyaz10@gmail.com>
2023-05-19 07:50:02 -0700

    Java Script variables

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName="Zachary";
+let lastName="Padovano";
+var age= 22;
 
-// Exercise 2
+let language ="JavaScript";
+let createdYear= 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize ="M";
+
+let title = "Name of the Wind";
+let author ="Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`Hello! My name is ${firstName} ${lastName} and I'm ${age} years old.`)
+
